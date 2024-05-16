6215824418f554726d81c8049dd8a48eb49ff191
JacobElbaz <jacobel@post.bgu.ac.il>
2024-05-16 15:44:33 +0300

    close on click

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import uniq from "@/assets/images/uniq.png";
-import burger from '@/assets/icons/menu.png';
+import burger from "@/assets/icons/menu.png";
 
 function NavBar() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -12,7 +12,9 @@ function NavBar() {
   };
   return (
     <div className="navbar">
-      <Image height={50} src={uniq} alt="uniq"></Image>
+      <Link href={'/'} style={{display: 'flex', alignItems: 'center'}}>
+        <Image height={50} src={uniq} alt="uniq"></Image>
+      </Link>
       <ul className="navbar-links">
         <li>
           <Link href={"/"}>Home</Link>
@@ -25,18 +27,26 @@ function NavBar() {
         </li>
       </ul>
       <div className="burger-menu">
-        <button className="burger-button" onClick={handleToggleMenu}><Image src={burger} alt="burger" height={30}/></button>
-          <ul className={isOpen ? "menu-links" : "menu-links hidden"}>
-            <li>
-              <Link href={"/"}>Home</Link>
-            </li>
-            <li>
-              <Link href={"/nos-offres"}>Nos offres</Link>
-            </li>
-            <li>
-              <Link href={"#contact"}>Nous contacter</Link>
-            </li>
-          </ul>
+        <button className="burger-button" onClick={handleToggleMenu}>
+          <Image src={burger} alt="burger" height={30} />
+        </button>
+        <ul className={isOpen ? "menu-links" : "menu-links hidden"}>
+          <li>
+            <Link href={"/"} onClick={handleToggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href={"/nos-offres"} onClick={handleToggleMenu}>
+              Nos offres
+            </Link>
+          </li>
+          <li>
+            <Link href={"#contact"} onClick={handleToggleMenu}>
+              Nous contacter
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
