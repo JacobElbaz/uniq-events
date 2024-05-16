6e134989f53728ddd2b805bc0cebb4b7eebaebfa
ElieB-1012 <eliebrachapro@gmail.com>
2024-05-16 12:02:40 +0200

    Image

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Feature from "@/components/Feature";
 import Hero from "@/components/Hero";
 import img from "../assets/images/hero.jpg";
+import waze from "../assets/images/waze.jpg";
+import calendar from "../assets/images/calendar.jpg";
+
 import ReviewsContainer from "@/components/ReviewsContainer";
 import Link from "next/link";
 
@@ -10,9 +13,9 @@ export default function Home() {
     <main>
       <Hero/>
       <Feature header={"Faire-part personnalisé de l'événement :"} description={"Partagez votre amour avec style grâce à nos faire-parts personnalisés, conçus pour capturer l'essence de votre journée spéciale."} image={img}/>
-      <Feature inverted header={"Liens pratiques:"} description={"Facilitez la vie de vos invités avec des liens directs vers WhatsApp pour les messages et Waze pour les itinéraires, afin qu'ils puissent rejoindre votre mariage en toute simplicité."} image={img}/>
+      <Feature inverted header={"Liens pratiques:"} description={"Facilitez la vie de vos invités avec des liens directs vers WhatsApp pour les messages et Waze pour les itinéraires, afin qu'ils puissent rejoindre votre mariage en toute simplicité."} image={waze}/>
       <Feature header={"Formulaire de réponse pour les invités :"} description={"Ne laissez aucun invité sans réponse ! Notre formulaire de réponse intégré permet à vos proches de confirmer leur présence en un seul clic."} image={img}/>
-      <Feature inverted header={"Intégration au calendrier :"} description={"Ajoutez votre mariage à l'agenda de vos invités en un clin d'œil, pour qu'ils n'oublient jamais le jour où votre amour sera célébré."} image={img}/>
+      <Feature inverted header={"Intégration au calendrier :"} description={"Ajoutez votre mariage à l'agenda de vos invités en un clin d'œil, pour qu'ils n'oublient jamais le jour où votre amour sera célébré."} image={calendar}/>
       <Feature header={"Espace privé pour les mariés avec accès à la liste des invités et détails des réponses :"} description={"Profitez d'un espace exclusif pour les mariés, où vous pourrez suivre les réponses de vos invités et planifier votre grand jour en toute tranquillité."} image={img}/>
       <ReviewsContainer/>
       <div style={{padding: '5rem 1rem'}}>
